3f52c85d9d88b8a2ddb86de2add9dbaa2efed1e5
Vincent Pretre <vincent.pretre@gmail.com>
2017-08-30 14:43:01 +0200

    Another test for Chrome headless
    Based on: http://www.amihaiemil.com/2017/07/14/selenium-headless-chrome-travis.html

--- src/test/java/com/coffeemachine/SeleniumDriverGetter.java
+++ src/test/java/com/coffeemachine/SeleniumDriverGetter.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -23,6 +24,23 @@ public class SeleniumDriverGetter {
     }
 
     private WebDriver getLocalDriver(DesiredCapabilities caps) {
+        if (System.getenv("USE_HEADLESS_CHROME") != null) {
+          String chromePath = System.getenv("CHROME_PATH");
+          if (chromePath == null) {
+            chromePath = "/usr/bin/google-chrome-stable";
+          }
+
+          final ChromeOptions chromeOptions = new ChromeOptions();
+          chromeOptions.setBinary(chromePath);
+          chromeOptions.addArguments("--headless");
+          chromeOptions.addArguments("--disable-gpu");
+
+          caps.setJavascriptEnabled(true);
+          caps.setCapability(
+              ChromeOptions.CAPABILITY, chromeOptions
+          );
+        }
+
         return new ChromeDriver(caps);
     }
 
