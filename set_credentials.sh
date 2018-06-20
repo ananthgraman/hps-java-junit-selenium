cd510cad69622bd7b575be8c3e9d656addfe95df
Vincent Pretre <vincent.pretre@gmail.com>
2018-06-20 15:39:53 +0200

    And also test on an available coffee machine

--- set_credentials.sh
+++ set_credentials.sh
@@ -3,6 +3,7 @@
 if [ "$USE_CBT" = true ]; then
   export REMOTE_DRIVER_USERNAME=$CBT_USERNAME;
   export REMOTE_DRIVER_PASSWORD=$CBT_PASSWORD;
+  export COFFEE_MACHINE_LOCATION=https://hiptest.github.io/hps-java-junit-selenium/src/web/coffee_machine.html
 
   echo "Running test on CBT, using account; $REMOTE_DRIVER_USERNAME";
 fi
@@ -10,6 +11,7 @@ fi
 if [ "$USE_SAUCELABS" = true ]; then
   export REMOTE_DRIVER_USERNAME=$SAUCELABS_USERNAME;
   export REMOTE_DRIVER_PASSWORD=$SAUCELABS_PASSWORD;
+  export COFFEE_MACHINE_LOCATION=https://hiptest.github.io/hps-java-junit-selenium/src/web/coffee_machine.html
 
   echo "Running test on SauceLABS, using account; $REMOTE_DRIVER_USERNAME";
 
