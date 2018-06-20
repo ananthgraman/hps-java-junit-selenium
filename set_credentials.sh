25e2a0411df29851fbe3f600f04f192452ba1442
Vincent Pretre <vincent.pretre@gmail.com>
2018-06-20 15:37:09 +0200

    Better checks for exporting variables

--- set_credentials.sh
+++ set_credentials.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if $USE_CBT; then
+if [ "$USE_CBT" = true ]; then
   export REMOTE_DRIVER_USERNAME=$CBT_USERNAME;
   export REMOTE_DRIVER_PASSWORD=$CBT_PASSWORD;
 
   echo "Running test on CBT, using account; $REMOTE_DRIVER_USERNAME";
 fi
 
-if $USE_SAUCELABS; then
+if [ "$USE_SAUCELABS" = true ]; then
   export REMOTE_DRIVER_USERNAME=$SAUCELABS_USERNAME;
   export REMOTE_DRIVER_PASSWORD=$SAUCELABS_PASSWORD;
 
